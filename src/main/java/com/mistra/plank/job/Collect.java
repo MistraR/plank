f5d35d1ad040e7a8a8cdb4ae2cd76257234e718a
王瑞 <mistra@fiture.com>
2021-11-18 16:20:49 +0800

    feat: init

--- src/main/java/com/mistra/plank/job/Collect.java
+++ src/main/java/com/mistra/plank/job/Collect.java
@@ -2,33 +2,26 @@ package com.mistra.plank.job;
 
 import com.mistra.plank.mapper.DailyRecordMapper;
 import com.mistra.plank.mapper.DragonListMapper;
-import com.mistra.plank.mapper.HoldSharesMapper;
-import com.mistra.plank.mapper.TradeRecordMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 /**
+ * 描述
+ *
  * @author mistra@future.com
  * @date 2021/11/18
  */
 @Slf4j
 @Component
-public class Rocket implements CommandLineRunner {
+public class Collect implements CommandLineRunner {
 
     private DailyRecordMapper dailyRecordMapper;
 
     private DragonListMapper dragonListMapper;
 
-    private HoldSharesMapper holdSharesMapper;
-
-    private TradeRecordMapper tradeRecordMapper;
-
-    public Rocket(DailyRecordMapper dailyRecordMapper, DragonListMapper dragonListMapper, HoldSharesMapper holdSharesMapper, TradeRecordMapper tradeRecordMapper) {
+    public Collect(DailyRecordMapper dailyRecordMapper) {
         this.dailyRecordMapper = dailyRecordMapper;
-        this.dragonListMapper = dragonListMapper;
-        this.holdSharesMapper = holdSharesMapper;
-        this.tradeRecordMapper = tradeRecordMapper;
     }
 
     @Override
