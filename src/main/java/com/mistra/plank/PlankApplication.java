4acc70456d4ff5646feb1ff2baa8779d0561bf0a
王瑞 <mistra@fiture.com>
2021-11-18 15:22:40 +0800

    Initial commit

---
+++ src/main/java/com/mistra/plank/PlankApplication.java
@@ -0,0 +1,13 @@
+package com.mistra.plank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mistra/plank/PlankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistra.plank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
