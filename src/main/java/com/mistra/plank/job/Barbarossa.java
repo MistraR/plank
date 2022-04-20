d9e006a4298e16bf68f3c835d38a0c925c44b689
Valar Morghulis <842404548@qq.com>
2022-04-20 10:16:08 +0800

    feat(*): 修改监控股价的循环次数

--- src/main/java/com/mistra/plank/job/Barbarossa.java
+++ src/main/java/com/mistra/plank/job/Barbarossa.java
@@ -137,7 +137,7 @@ public class Barbarossa implements CommandLineRunner {
                 List<String> haveStockList = Arrays.asList(haveStock.split(","));
                 List<Stock> stocks = stockMapper.selectList(new QueryWrapper<Stock>().in("name", haveStockList));
                 HashMap<String, String> price = new HashMap<>();
-                int count = 100;
+                int count = 500;
                 while (true) {
                     for (Stock stock : stocks) {
                         String url = plankConfig.getXueQiuStockDetailUrl();
