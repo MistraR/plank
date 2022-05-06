3a99d4f0ea8f08bc9e907fd8caf67c4eefd27eff
Valar Morghulis <842404548@qq.com>
2022-05-06 17:34:24 +0800

    feat(*): sql

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,6 +1,6 @@
 SELECT count(*)
 FROM daily_record
-WHERE DATE_FORMAT(date, '%Y-%m-%d') = '2022-04-22';
+WHERE DATE_FORMAT(date, '%Y-%m-%d') = '2022-05-05';
 SELECT count(*)
 FROM daily_record
 WHERE DATE_FORMAT(date, '%Y-%m-%d') = DATE_FORMAT(now(), '%Y-%m-%d');
@@ -56,4 +56,16 @@ ORDER BY code;
 UPDATE stock
 SET purchase_price=40
 WHERE `name` = '宁德时代';
+
+SELECT name, current_price
+FROM stock
+WHERE `name` in ('泸州老窖', '五粮液', '舍得酒业', '洋河股份', '山西汾酒', '酒鬼酒', '隆基股份', '中环股份', '晶澳科技', '通威股份', '阳光电源', '合盛硅业',
+                 '卓胜微', '韦尔股份', '兆易创新', '圣邦股份', '紫光国微', '药明康德', '泰格医药', '迈瑞医疗', '凯莱英', '智飞生物', '康龙化成', '博腾股份', '宁德时代',
+                 '天齐锂业', '赣锋锂业', '亿纬锂能', '盐湖股份',
+                 '比亚迪', '立讯精密', '歌尔股份', '万华化学', '美的集团', '汇川技术', '海康威视', '伊利股份', '东方财富', '爱尔眼科', '爱美客', '贝泰妮', '珀莱雅',
+                 '贵州茅台')
+ORDER BY name asc
+
+
+
 
