a857ad01d905cae98235a401c5c121ea486f767e
MistraR <842404548@qq.com>
2022-11-04 16:03:57 +0800

    feat(*): web

--- stock-web/static/js/modules/report.js
+++ stock-web/static/js/modules/report.js
@@ -1,7 +1,7 @@
 function renderReportMenu(current) {
   var arr = [
-    { id: 1, title: '股票列表', url: '/report/stockList.html' },
-    { id: 2, title: '每日统计', url: '/report/dailyList.html' }
+    { id: 1, title: '股票列表', url: '/report/stockList.html' }
+    // { id: 2, title: '每日统计', url: '/report/dailyList.html' }
   ];
 
   renderMenu(arr, '.menu-nav', current);
--- stock-web/static/js/util/frame.js
+++ stock-web/static/js/util/frame.js
@@ -2,7 +2,7 @@ var pageConfiguration = {
   needAuth: false
 };
 
-$(function() {
+$(function () {
   if (pageConfiguration.needAuth) {
     if (!isLocalAuth()) {
       LocationUtil.goto('/user/login.html');
@@ -19,12 +19,12 @@ $(function() {
 
   function renderHead() {
     var content = '<div class="innerBox"><ul class="top-head">';
-    content += '<li><a href="/">首页</a></li>';
+    content += '<li><a href="/"><h2>首页</h2></a></li>';
     if (!isLocalAuth()) {
-      content += '<li><a href="/user/login.html">登录</a></li>';
+      content += '<li><a href="/user/login.html"><h2>登录</h2></a></li>';
     } else {
-      content += '<li><a href="/user/profile.html">个人中心</a></li>';
-      content += '<li><a href="/user/logout.html">注销</a></li>';
+      content += '<li><a href="/user/profile.html"><h2>个人中心</h2></a></li>';
+      content += '<li><a href="/user/logout.html"><h2>注销</h2></a></li>';
     }
     content += '</ul></div>';
     $('#head').html(content);
@@ -45,12 +45,12 @@ $(function() {
 
 function renderMenu(arr, selector, current) {
   var content = '';
-  $.each(arr, function(index, item) {
+  $.each(arr, function (index, item) {
     if (item.state !== 0) {
       if (current === item.id) {
-        content += '<li class="current"><a href="javascript:void(0);">' + item.title +'</a></li>';
+        content += '<li class="current"><a href="javascript:void(0);">' + item.title + '</a></li>';
       } else {
-        content += '<li ><a href="' + item.url + '">' + item.title +'</a></li>';
+        content += '<li ><a href="' + item.url + '">' + item.title + '</a></li>';
       }
     }
   });
