dd913bafa6d3884ec0c1cf2aaff1c20075e0ffef
MistraR <842404548@qq.com>
2023-03-22 14:14:17 +0800

    feat(*): log

--- src/main/java/com/mistra/plank/job/Barbarossa.java
+++ src/main/java/com/mistra/plank/job/Barbarossa.java
@@ -283,7 +283,7 @@ public class Barbarossa implements CommandLineRunner {
                 }
                 realTimePrices.removeIf(e -> STOCK_TRACK_MAP.containsKey(e.getName()) && STOCK_TRACK_MAP.get(e.getName()).getShareholding());
                 List<StockRealTimePrice> stockRealTimePrices = realTimePrices.stream().filter(e ->
-                        e.getPurchaseRate() >= -1).collect(Collectors.toList());
+                        e.getPurchaseRate() >= -2).collect(Collectors.toList());
                 if (CollectionUtils.isNotEmpty(stockRealTimePrices)) {
                     log.error("------------------------------ Track ------------------------------");
                     stockRealTimePrices.forEach(e -> Barbarossa.log.warn(convertLog(e)));
@@ -307,7 +307,6 @@ public class Barbarossa implements CommandLineRunner {
                             .map(Stock::getName).collect(Collectors.toList())));
                 }
                 realTimePrices.clear();
-                Thread.sleep(1000);
             }
         } catch (Exception e) {
             e.printStackTrace();
